e765c2709be849def7011c1bb7877d6b3d3aefbf
CholponLee <cholpon_lee@mail.ru>
2021-03-17 22:59:37 +0600

    SkillFactory: QAP-13. Practicum 16.7.1

---
+++ Test_docs/practicum_16_8_2/test_rectangle.py
@@ -0,0 +1,18 @@
+from rectangle import Rectangle
+
+r1 = Rectangle(10, 5)
+
+print('r1.width =', r1.width)
+print('r1.height =', r1.height)
+print('r1.getWidth =', r1.get_width())
+print('r1.height =', r1.get_height())
+print('r1.getArea =', r1.get_area())
+
+
+r2 = Rectangle(15, 7)
+
+print('r2.width =', r2.width)
+print('r2.height =', r2.height)
+print('r2.getWidth =', r2.get_width())
+print('r2.height =', r2.get_height())
+print('r2.getArea =', r2.get_area())
